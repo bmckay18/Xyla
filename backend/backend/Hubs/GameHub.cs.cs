b1e0b6bc7fd45b719d8d702c8fef4c903692d104
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2026-07-26 18:40:09 +1000

    Initial commit of backend logic

---
+++ backend/backend/Hubs/GameHub.cs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Backend.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task JoinLobby(string lobbyId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, lobbyId);
+        }
+    }
+}
---
+++ backend/backend/Services/Lobbies/ILobbyService.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Services.Lobbies.Models;
+
+namespace Backend.Services.Lobbies
+{
+    public interface ILobbyService
+    {
+        Lobby CreateLobby(string host);
+        Lobby? GetLobby(Guid id);
+    }
+}
