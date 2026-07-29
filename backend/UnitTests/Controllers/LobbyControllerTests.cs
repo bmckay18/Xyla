275af7ddabfa1d11ac83b2f47ea9df94e57efc30
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2026-07-29 23:41:01 +1000

    Create unit test project and add lobby service and controller tests

---
+++ backend/UnitTests/Controllers/LobbyControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using Backend.Controllers;
+using Backend.Controllers.Models;
+using Backend.Services.Lobbies;
+using Backend.Services.Lobbies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Controllers
+{
+    public class LobbyControllerTests
+    {
+        private Mock<ILobbyService> _lobbyServiceMock;
+        private LobbyController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _lobbyServiceMock = new Mock<ILobbyService>();
+
+            _controller = new LobbyController(_lobbyServiceMock.Object);
+        }
+
+        [Test]
+        public void CreateLobby_CallsLobbyServiceCreateLobbyMethod_WithCorrectParameters()
+        {
+            var request = new CreateLobbyRequest
+            {
+                HostName = "user1",
+                Password = "password"
+            };
+
+            _controller.CreateLobby(request);
+
+            _lobbyServiceMock.Verify(s => s.CreateLobby(request.HostName, request.Password), Times.Once);
+        }
+
+        [Test]
+        public void GetLobby_ReturnsOkResult_WhenLobbyExists()
+        {
+            var lobbyId = Guid.NewGuid();
+            var mockResult = new LobbyDetailsDto
+            {
+                HostId = "1",
+                Players = new List<Player> { new Player { Name = "User1" } }
+            };
+
+            _lobbyServiceMock.Setup(r => r.GetLobby(It.IsAny<Guid>())).Returns(mockResult);
+
+            var result = _controller.GetLobby(lobbyId);
+            var okResult = result.Result as OkObjectResult;
+
+            Assert.That(result.Result, Is.TypeOf<OkObjectResult>());
+            _lobbyServiceMock.Verify(r => r.GetLobby(It.IsAny<Guid>()), Times.Once);
+            Assert.That(okResult!.Value, Is.EqualTo(mockResult));
+        }
+
+        [Test]
+        public void GetLobby_ReturnsNotFoundResult_WhenLobbyDoesNotExist()
+        {
+            var lobbyId = Guid.NewGuid();
+
+            _lobbyServiceMock.Setup(r => r.GetLobby(It.IsAny<Guid>())).Returns((LobbyDetailsDto?)null);
+
+            var result = _controller.GetLobby(lobbyId);
+
+            Assert.That(result.Result, Is.TypeOf<NotFoundResult>());
+            _lobbyServiceMock.Verify(r => r.GetLobby(It.IsAny<Guid>()), Times.Once);
+        }        
+    }
+}
---
+++ backend/UnitTests/Services/LobbyServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Backend.Mapping;
+using Backend.Services.Lobbies;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace UnitTests.Services
+{
+    [TestFixture]
+    public class LobbyServiceTests
+    {
+        private ILobbyService _service;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<LobbyMappingProfile>();
+            }, NullLoggerFactory.Instance);
+
+            _mapper = mapperConfig.CreateMapper();
+
+            _service = new LobbyService(_mapper);
+        }
+
+        [Test]
+        public void CreateLobby_AssignsValidGuidsToLobbyAndPlayer_WhenValidDataIsPresent()
+        {
+            var hostName = "user1";
+            var password = "test123";
+
+            var lobby = _service.CreateLobby(hostName, password);
+
+            Assert.That(lobby, Is.Not.Null);
+            Assert.That(lobby.LobbyId, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(lobby.PlayerId, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void CreateLobby_CreatesAndStoresLobbyInMemory()
+        {
+            var hostName = "user3";
+
+            var lobby = _service.CreateLobby(hostName, null);
+            var retrievedLobby = _service.GetLobby(lobby.LobbyId);
+
+            Assert.That(lobby, Is.Not.Null);
+            Assert.That(retrievedLobby, Is.Not.Null);
+            Assert.That(lobby.PlayerId.ToString(), Is.EqualTo(retrievedLobby.HostId));
+            Assert.That(retrievedLobby.Players.Count, Is.EqualTo(1));
+            Assert.That(retrievedLobby.Players[0].Name, Is.EqualTo(hostName));
+        }
+    }
+}
